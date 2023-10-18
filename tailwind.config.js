9b7865726ae0e73a2b7cdc4c88c17ae6530c233a
Nashiuz Zaman <nashiuz.zaman@gmail.com>
2023-10-18 14:39:54 +0600

    Change some settings on Tailwind config file

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,32 @@
 export default {
   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      screens: {
+        "2xsm": "360px",
+        xsm: "480px",
+      },
+      margin: {
+        sectionGapLg: "11rem",
+        sectionGapMd: "9rem",
+        elementGapSm: "1.4rem",
+        elementGapMd: "3rem",
+      },
+
+      colors: {
+        primary: "#b91c1c",
+        primaryLight: "#c03333",
+        textPrimary: "#1C1B1B",
+        textMediumLight: "#1c1b1bcc",
+        textLight: "#1c1b1b99",
+      },
+      fontFamily: {
+        poppins: "'Poppins', sans-serif",
+      },
+      borderRadius: {
+        default: "8px",
+      },
+    },
   },
   plugins: [],
 };
