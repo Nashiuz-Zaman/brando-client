155e2d648304c173b4c6a5952773ad841b29fb97
Nashiuz Zaman <nashiuz.zaman@gmail.com>
2023-10-18 14:36:47 +0600

    Client side project start

---
+++ src/router/router.jsx
@@ -0,0 +1,18 @@
+// react router imports
+import { createBrowserRouter } from "react-router-dom";
+
+// app component
+import App from "../App";
+
+// page components
+import Home from "../components/pages/Home/Home";
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <App />,
+    children: [{ path: "/", element: <Home /> }],
+  },
+]);
+
+export default router;
