3f01552d0b0737a218ba545a00187619b20bab0b
Nashiuz Zaman <nashiuz.zaman@gmail.com>
2023-10-19 14:54:05 +0600

    Add nav jsx tag to both navigations

--- src/components/shared/LargeScreenNav/LargeScreenNav.jsx
+++ src/components/shared/LargeScreenNav/LargeScreenNav.jsx
@@ -6,41 +6,43 @@ import { NavLink } from "react-router-dom";
 
 const LargeScreenNav = ({ navigationOptions }) => {
   return (
-    // these btns are coming from the navigation options data file
-    <ul className="flex gap-6 ">
-      {navigationOptions &&
-        navigationOptions.map((option) => {
-          return (
-            <li key={option.id}>
-              <NavLink
-                className="leading-[normal] px-2 border-x-2 border-transparent hover:border-white text-white font-medium transition-all duration-200"
-                to={option.url}
-              >
-                {option.text}
-              </NavLink>
-            </li>
-          );
-        })}
+    // these links are coming from the navigation options data file
+    <>
+      <ul className="flex gap-6 ">
+        {navigationOptions &&
+          navigationOptions.map((option) => {
+            return (
+              <li key={option.id}>
+                <NavLink
+                  className="leading-[normal] px-2 border-x-2 border-transparent hover:border-white text-white font-medium transition-all duration-200"
+                  to={option.url}
+                >
+                  {option.text}
+                </NavLink>
+              </li>
+            );
+          })}
 
-      {/* login and register btns */}
-      {/* this part will always remain here for the sign in and sign out functionality */}
-      <li>
-        <NavLink
-          className="leading-[normal] px-2 border-x-2 border-transparent hover:border-white text-white font-medium transition-all duration-200"
-          to="/login"
-        >
-          Log In
-        </NavLink>
-      </li>
-      <li>
-        <NavLink
-          className="leading-[normal] px-2 border-x-2 border-transparent hover:border-white text-white font-medium transition-all duration-200"
-          to="/login"
-        >
-          Register
-        </NavLink>
-      </li>
-    </ul>
+        {/* login and register btns */}
+        {/* this part will always remain here for the sign in and sign out functionality */}
+        <li>
+          <NavLink
+            className="leading-[normal] px-2 border-x-2 border-transparent hover:border-white text-white font-medium transition-all duration-200"
+            to="/login"
+          >
+            Log In
+          </NavLink>
+        </li>
+        <li>
+          <NavLink
+            className="leading-[normal] px-2 border-x-2 border-transparent hover:border-white text-white font-medium transition-all duration-200"
+            to="/login"
+          >
+            Register
+          </NavLink>
+        </li>
+      </ul>
+    </>
   );
 };
 
--- src/components/shared/MobileNav/MobileNav.jsx
+++ src/components/shared/MobileNav/MobileNav.jsx
@@ -19,7 +19,7 @@ const MobileNav = ({
   logOutFunction = null,
 }) => {
   return (
-    <div
+    <nav
       className={`block lg:hidden h-screen fixed top-0 right-0 w-full -translate-x-full transition-all duration-300 z-20 ${
         openState ? "!translate-x-0" : ""
       } text-center p-8 bg-gradient-to-br from-primary to-primaryLight`}
@@ -85,7 +85,7 @@ const MobileNav = ({
           />
         </div>
       )}
-    </div>
+    </nav>
   );
 };
 
