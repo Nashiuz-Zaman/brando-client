b6e0ee4a2e2b1b0fe3934d3891846791a9d930b2
Nashiuz Zaman <nashiuz.zaman@gmail.com>
2023-12-18 19:34:40 +0600

    Change the text color of SectionHeading

--- src/components/shared/SectionHeading/SectionHeading.jsx
+++ src/components/shared/SectionHeading/SectionHeading.jsx
@@ -3,9 +3,7 @@ import PropTypes from "prop-types";
 
 const SectionHeading = ({ text, modifyClasses = "" }) => {
   return (
-    <h2
-      className={`text-3xl text-primary text-center font-bold ${modifyClasses}`}
-    >
+    <h2 className={`text-3xl text-center font-bold ${modifyClasses}`}>
       {text}
     </h2>
   );
