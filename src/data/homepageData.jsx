83ed9f4a6d9257f928d8084e046bb0519afc8638
Nashiuz Zaman <nashiuz.zaman@gmail.com>
2023-12-18 19:10:12 +0600

    Add home page UI data into homepageData file

---
+++ src/data/homepageData.jsx
@@ -0,0 +1,25 @@
+export const bannerTextObj = {
+  heading: (
+    <>
+      <span className="text-primary">Brando:</span> Elevate Your{" "}
+      <br className="hidden xsm:inline-block" />
+      Style with Iconic Brands
+    </>
+  ),
+
+  description: (
+    <>
+      Discover the latest trends in footwear and fashion from renowned brands
+      <br className="hidden xsm:inline-block lg:hidden 2xl:inline-block" /> like
+      Nike, H&M, Levi&apos;s, Zara, and more.
+    </>
+  ),
+  btnText: "Shop Now",
+};
+
+export const carouselImages = [
+  { id: 0, imageSource: "https://i.ibb.co/sg3FXgY/hnm.webp" },
+  { id: 1, imageSource: "https://i.ibb.co/DQkJjTf/nike.webp" },
+  { id: 2, imageSource: "https://i.ibb.co/zsXdz07/zara.webp" },
+  { id: 3, imageSource: "https://i.ibb.co/58Fkb8P/puma.webp" },
+];
