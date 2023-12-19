0192dd0235ae1d30cc9594b4a7d98c267415bc1f
Nashiuz Zaman <nashiuz.zaman@gmail.com>
2023-12-20 04:52:49 +0600

    Change ButtonBtn code structure, refactor code and add a new theme prop to it

--- src/components/shared/ButtonBtn/ButtonBtn.jsx
+++ src/components/shared/ButtonBtn/ButtonBtn.jsx
@@ -3,18 +3,45 @@ import PropTypes from "prop-types";
 
 const ButtonBtn = ({
   text,
-  onClickFunction,
-  outlined = false,
+  onClickFunction = null,
+  colorTheme = "",
   modifyClasses = "",
+  theme = "light",
 }) => {
+  // common classes
+  const outlinedClasses =
+    "bg-transparent border border-gray-300 hover:border-white text-gray-300 hover:text-white";
+
+  const oulinedPrimaryClasses = `bg-transparent border ${
+    theme === "dark"
+      ? "border-white text-white"
+      : "border-primary text-primary "
+  }  hover:text-primaryDark hover:border-primaryDark`;
+
+  const primaryClasses = `border ${
+    theme === "dark"
+      ? "bg-primaryDarkTheme border-primaryDarkTheme"
+      : "bg-primary border-primary"
+  } hover:border-primaryDark hover:bg-primaryDark text-white`;
+
+  const blackClasses =
+    "bg-blackLight border border-blackLight hover:bg-textPrimary hover:border-textPrimary text-white";
+
+  const allClasses = `block w-max transition-all duration-default rounded-full text-center px-6 py-2 3xl:text-xl 2xl:py-3 3xl:py-4 ${modifyClasses}`;
+
   return (
     <button
       onClick={onClickFunction}
+      // decide the design of button according to the props
       className={`${
-        outlined
-          ? "bg-transparent border border-white text-white hover:bg-[rgba(255,255,255,0.5)]"
-          : "bg-primary border border-primary hover:border-primaryLight hover:bg-primaryLight text-white"
-      } block transition-all duration-300 rounded-default px-5 py-2 text-lg ${modifyClasses}`}
+        colorTheme === "outlined"
+          ? outlinedClasses
+          : colorTheme === "outlinedPrimary"
+          ? oulinedPrimaryClasses
+          : colorTheme === "black"
+          ? blackClasses
+          : primaryClasses
+      } ${allClasses}`}
     >
       {text}
     </button>
@@ -24,8 +51,9 @@ const ButtonBtn = ({
 ButtonBtn.propTypes = {
   text: PropTypes.string.isRequired,
   onClickFunction: PropTypes.func,
-  outlined: PropTypes.bool,
+  colorTheme: PropTypes.string,
   modifyClasses: PropTypes.string,
+  theme: PropTypes.string,
 };
 
 export default ButtonBtn;
