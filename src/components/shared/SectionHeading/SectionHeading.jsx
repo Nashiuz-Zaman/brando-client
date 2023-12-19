3520a08a9e65ee795a8b85cf2bf65de2adb7c876
Nashiuz Zaman <nashiuz.zaman@gmail.com>
2023-12-20 04:11:18 +0600

    Implement text color change inside SectionHeading component according to the theme

--- src/components/shared/SectionHeading/SectionHeading.jsx
+++ src/components/shared/SectionHeading/SectionHeading.jsx
@@ -1,9 +1,18 @@
 // react imports
 import PropTypes from "prop-types";
 
+// theme
+import useThemeProvider from "../../../hooks/useThemeProvider";
+
 const SectionHeading = ({ text, modifyClasses = "" }) => {
+  const { theme } = useThemeProvider();
+
   return (
-    <h2 className={`text-3xl text-center font-bold ${modifyClasses}`}>
+    <h2
+      className={`text-3xl text-center font-bold ${
+        theme === "dark" ? "text-white" : "text-textPrimary"
+      } ${modifyClasses}`}
+    >
       {text}
     </h2>
   );
