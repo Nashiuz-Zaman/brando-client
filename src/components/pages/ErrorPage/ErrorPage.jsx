1e0ccf6514a87ecac7da8f746a0a902bf508e805
Nashiuz Zaman <nashiuz.zaman@gmail.com>
2024-01-25 18:41:01 +0600

    Add a new ErrorPage to project and delete the old one

--- src/components/pages/ErrorPage/ErrorPage.jsx
+++ src/components/pages/ErrorPage/ErrorPage.jsx
@@ -3,17 +3,23 @@ import LinkBtn from "../../shared/LinkBtn/LinkBtn";
 
 const ErrorPage = () => {
   return (
-    <div className="my-sectionGapLg">
-      {/* error message */}
-      <p className="text-center text-3xl font-semibold mb-4">
-        Oops we have hit a dead end!
-      </p>
-      <p className="text-center text-3xl font-semibold">
-        No resource found for this route.
-      </p>
+    <div className="min-h-screen flex justify-center items-center py-sectionGapLg">
+      <div className="max-w-[90rem] 3xl:max-w-full px-6 md:px-8 lg:px-12 2xl:px-0 mx-auto 3xl:px-[7rem]">
+        <div>
+          {/* error message */}
 
-      {/* link to go back to home */}
-      <LinkBtn text="Go to Home" url="/" modifyClasses="w-max mx-auto mt-8" />
+          <p className="text-center text-3xl font-semibold">
+            404 | No page found
+          </p>
+
+          {/* link to go back to home */}
+          <LinkBtn
+            text="Go to Home"
+            url="/"
+            modifyClasses="w-max mx-auto mt-8"
+          />
+        </div>
+      </div>
     </div>
   );
 };
