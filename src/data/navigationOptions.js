f6d621be5b408e336fe649074ea3c0328ab5f957
Nashiuz Zaman <nashiuz.zaman@gmail.com>
2024-01-25 18:35:38 +0600

    Refactor navigation options data

--- src/data/navigationOptions.js
+++ src/data/navigationOptions.js
@@ -1,5 +1,5 @@
 export const navigationOptions = [
   { id: 1, text: "Home", url: "/" },
-  { id: 2, text: "Add Product", url: "/add-product" },
-  { id: 3, text: "My Cart", url: "/my-cart" },
+  // { id: 2, text: "Add Product", url: "/add-product" },
+  { id: 2, text: "My Cart", url: "/my-cart" },
 ];
